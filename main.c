a3bb80a0b26c016eed3587687f6e24cb88ca60fe
Pedro Gaya <pedrogaya@gmail.com>
2020-04-22 15:23:42 -0300

    Applied array pointers

--- main.c
+++ main.c
@@ -5,22 +5,22 @@
 #include <dos.h>
 #include <dir.h>
 int tries;
-clock_t t;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void printSudoku(int game[9][9]){
 	system("cls");
-	printf("Try %d\n\n",tries);
+	printf("\tTry %d\n\n\n",tries);
 	int i,j;
 	for(i=0;i<9;i++){
+		printf("\t\t");
 		for(j=0;j<9;j++){
 			if(game[i][j]==0) printf("  ");
 			else printf("%d ",game[i][j]);
 			if(j==2) printf("  ");
-			if(j==5) printf("  ");
-			if(j==8) printf("\n");
+			else if(j==5) printf("  ");
+			else if(j==8) printf("\n");
 			if(i==2 && j==8) printf("\n");
-			if(i==5 && j==8) printf("\n");
+			else if(i==5 && j==8) printf("\n");
 		}
 	}
 }
@@ -45,27 +45,23 @@ int isSolved(int game[9][9]){
 }
 
 void solveSudoku(int game[9][9]){
+	
 	int i,j,guess;
 	for(i=0;i<9;i++){
 		for(j=0;j<9;j++){
-			if(isSolved(game)){
-				printSudoku(game);
-				t = clock() - t;
-				double taken = ((double)t)/CLOCKS_PER_SEC;
-				printf("\n%f segundos\n",taken);
-				getch();
-			}
+			
 			if(game[i][j] == 0){
 				guess = 1;
 				while(guess <= 9){
 					tries++;
 					if(isValid(game,i,j,guess)){
 						//printSudoku(game);
-						game[i][j] = guess;
+						*(*(game+i)+j) = guess;
 						solveSudoku(game);
+						if(isSolved(game)) return;
 					}
 					if(guess==9 && !isValid(game,i,j,guess)){
-						game[i][j] = 0;
+						*(*(game+i)+j) = 0;
 						return;
 					}
 					guess++;
@@ -184,8 +180,7 @@ void printWelcome(){
 	printf("\t\t\t\t\t\tPRESS ANY KEY TO START.\n");	
 }
 
-
-int main(void) {
+void setupWindow(){
 	WORD wColor;
     HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_SCREEN_BUFFER_INFO csbi;
@@ -199,8 +194,13 @@ int main(void) {
 	keybd_event(VK_RETURN,0x1c,0,0);
 	keybd_event(VK_RETURN,0x1c,KEYEVENTF_KEYUP,0);
 	keybd_event(VK_MENU,0x38,KEYEVENTF_KEYUP,0);
+}
+
+int main(void) {
+	setupWindow();
+	clock_t t;
 	char line[9];
-	int countline,i,a,b,game[9][9];
+	int countline,i,a,b;
 	countline = 1;
 	/*
 	int game[9][9] = {{0,6,0,3,0,0,8,0,4},
@@ -213,6 +213,7 @@ int main(void) {
 			{0,0,0,0,6,0,5,2,3},
 			{1,0,2,0,0,9,0,8,0}
 			};
+	*/
 	int game2[9][9] = {{6,2,9,1,7,8,4,3,5},
 					   {8,4,5,3,6,2,7,9,1},
 					   {1,3,7,0,9,4,8,0,6},
@@ -222,9 +223,9 @@ int main(void) {
 					   {9,6,3,8,5,7,1,4,2},
 					   {5,8,4,9,2,1,3,6,7},
 					   {7,1,2,4,3,6,9,5,8}};
-	*/
 	printWelcome();
 	getch();
+	/*
 	printf("Please type in the sudoku you want to solve line by line (0 for blank):\n\n");
 	while(countline < 10){
 		scanf("%s",line);
@@ -239,9 +240,15 @@ int main(void) {
 		}
 		printf("\n");
 	}
+	*/
 	system("cls");
 	printf("Solving\n");
 	t = clock();
-	solveSudoku(game);
+	solveSudoku(game2);
+	t = clock() - t;
+	double taken = ((double)t)/CLOCKS_PER_SEC;
+	printSudoku(game2);
+	printf("\n\n\t%f segundos\n",taken);
+	getch();
 	return 0;
 }
