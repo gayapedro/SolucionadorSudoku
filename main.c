af29234b29cb46d2fabb65b232467138df67ecff
Pedro Gaya <pedrogaya@gmail.com>
2020-04-22 14:17:10 -0300

    Created welcome screen

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
+#include <dos.h>
+#include <dir.h>
 int tries;
 clock_t t;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
@@ -144,7 +147,58 @@ int isValid(int game[9][9],int i,int j,int value){
 	return 1;
 }
 
+void printWelcome(){
+	system("cls");
+	printf("\n\t   SSSSSSSSSSSSSSS                               d::::::d                 kkkkkkkk                             \n");
+	printf("\t SS:::::::::::::::S                              d::::::d                 k::::::k                             \n");
+	printf("\tS:::::SSSSSS::::::S                              d::::::d                 k::::::k                             \n");
+	printf("\tS:::::S     SSSSSSS                              d:::::d                  k::::::k                             \n");
+	printf("\tS:::::S            uuuuuu    uuuuuu      ddddddddd:::::d    ooooooooooo    k:::::k    kkkkkkkuuuuuu    uuuuuu  \n");
+	printf("\tS:::::S            u::::u    u::::u    dd::::::::::::::d  oo:::::::::::oo  k:::::k   k:::::k u::::u    u::::u  \n");
+	printf("\t S::::SSSS         u::::u    u::::u   d::::::::::::::::d o:::::::::::::::o k:::::k  k:::::k  u::::u    u::::u  \n");
+	printf("\t  SS::::::SSSSS    u::::u    u::::u  d:::::::ddddd:::::d o:::::ooooo:::::o k:::::k k:::::k   u::::u    u::::u  \n");
+	printf("\t    SSS::::::::SS  u::::u    u::::u  d::::::d    d:::::d o::::o     o::::o k::::::k:::::k    u::::u    u::::u  \n");
+	printf("\t       SSSSSS::::S u::::u    u::::u  d:::::d     d:::::d o::::o     o::::o k:::::::::::k     u::::u    u::::u  \n");
+	printf("\t            S:::::Su::::u    u::::u  d:::::d     d:::::d o::::o     o::::o k:::::::::::k     u::::u    u::::u  \n");
+	printf("\t            S:::::Su:::::uuuu:::::u  d:::::d     d:::::d o::::o     o::::o k::::::k:::::k    u:::::uuuu:::::u  \n");
+	printf("\tSSSSSSS     S:::::Su:::::::::::::::uud::::::ddddd::::::ddo:::::ooooo:::::ok::::::k k:::::k   u:::::::::::::::uu\n");
+	printf("\tS::::::SSSSSS:::::S u:::::::::::::::u d:::::::::::::::::do:::::::::::::::ok::::::k  k:::::k   u:::::::::::::::u\n");
+	printf("\tS:::::::::::::::SS   uu::::::::uu:::u  d:::::::::ddd::::d oo:::::::::::oo k::::::k   k:::::k   uu::::::::uu:::u\n");
+	printf("\t SSSSSSSSSSSSSSS       uuuuuuuu  uuuu   ddddddddd   ddddd   ooooooooooo   kkkkkkkk    kkkkkkk    uuuuuuuu  uuuu\n\n");
+	printf("\t   SSSSSSSSSSSSSSS                  lllllll                                                              \n");
+	printf("\t SS:::::::::::::::S                 l:::::l                                                              \n");
+	printf("\tS:::::SSSSSS::::::S                 l:::::l                                                              \n");
+	printf("\tS:::::S     SSSSSSS                 l:::::l                                                              \n");
+	printf("\tS:::::S               ooooooooooo    l::::lvvvvvvv           vvvvvvv eeeeeeeeeeee    rrrrr   rrrrrrrrr   \n");
+	printf("\tS:::::S             oo:::::::::::oo  l::::l v:::::v         v:::::vee::::::::::::ee  r::::rrr:::::::::r  \n");
+	printf("\t S::::SSSS         o:::::::::::::::o l::::l  v:::::v       v:::::ve::::::eeeee:::::eer:::::::::::::::::r \n");
+	printf("\t  SS::::::SSSSS    o:::::ooooo:::::o l::::l   v:::::v     v:::::ve::::::e     e:::::err::::::rrrrr::::::r\n");
+	printf("\t    SSS::::::::SS  o::::o     o::::o l::::l    v:::::v   v:::::v e:::::::eeeee::::::e r:::::r     r:::::r\n");
+	printf("\t       SSSSSS::::S o::::o     o::::o l::::l     v:::::v v:::::v  e:::::::::::::::::e  r:::::r     rrrrrrr\n");
+	printf("\t            S:::::So::::o     o::::o l::::l      v:::::v:::::v   e::::::eeeeeeeeeee   r:::::r            \n");
+	printf("\t            S:::::So::::o     o::::o l::::l       v:::::::::v    e:::::::e            r:::::r            \n");
+	printf("\tSSSSSSS     S:::::So:::::ooooo:::::ol::::::l       v:::::::v     e::::::::e           r:::::r            \n");
+	printf("\tS::::::SSSSSS:::::So:::::::::::::::ol::::::l        v:::::v       e::::::::eeeeeeee   r:::::r            \n");
+	printf("\tS:::::::::::::::SS  oo:::::::::::oo l::::::l         v:::v         ee:::::::::::::e   r:::::r            \n");
+	printf("\t SSSSSSSSSSSSSSS      ooooooooooo   llllllll          vvv            eeeeeeeeeeeeee   rrrrrrr            \n\n\n\n");
+	printf("\t\t\t\t\t\tPRESS ANY KEY TO START.\n");	
+}
+
+
 int main(void) {
+	WORD wColor;
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    if(GetConsoleScreenBufferInfo(hStdOut, &csbi)){
+    	wColor = (csbi.wAttributes & 0xF0) + (1 & 0x0F);
+        SetConsoleTextAttribute(hStdOut, wColor);
+     }
+	HWND hWnd;
+    SetConsoleTitle("SudokuSolver");
+    keybd_event(VK_MENU,0x38,0,0);
+	keybd_event(VK_RETURN,0x1c,0,0);
+	keybd_event(VK_RETURN,0x1c,KEYEVENTF_KEYUP,0);
+	keybd_event(VK_MENU,0x38,KEYEVENTF_KEYUP,0);
 	char line[9];
 	int countline,i,a,b,game[9][9];
 	countline = 1;
@@ -169,6 +223,8 @@ int main(void) {
 					   {5,8,4,9,2,1,3,6,7},
 					   {7,1,2,4,3,6,9,5,8}};
 	*/
+	printWelcome();
+	getch();
 	printf("Please type in the sudoku you want to solve line by line (0 for blank):\n\n");
 	while(countline < 10){
 		scanf("%s",line);
